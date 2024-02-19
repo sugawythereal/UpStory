4c4ad5e1ef83d9702c9029e46586c33dc9528479
sugaa <159729456+sugawythereal@users.noreply.github.com>
2024-02-19 00:23:39 +0000

    update 2
    - qb-clothing
    + illenium
    + ox_lib
    +update fxmanifest on some scripts

--- UpStoryDB.sql
+++ UpStoryDB.sql
@@ -14,57 +14,434 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping database structure for upstorydb
+CREATE DATABASE IF NOT EXISTS `upstorydb` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci */;
+USE `upstorydb`;
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.apartments
+CREATE TABLE IF NOT EXISTS `apartments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `citizenid` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.apartments: ~1 rows (approximately)
+INSERT INTO `apartments` (`id`, `name`, `type`, `label`, `citizenid`) VALUES
+	(1, 'apartment35754', 'apartment3', 'Integrity Way 5754', 'BKD55994');
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.bank_accounts
+CREATE TABLE IF NOT EXISTS `bank_accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `account_name` varchar(50) DEFAULT NULL,
+  `account_balance` int(11) NOT NULL DEFAULT 0,
+  `account_type` enum('shared','job','gang') NOT NULL,
+  `users` longtext DEFAULT '[]',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `account_name` (`account_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.bank_accounts: ~0 rows (approximately)
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.bank_statements
+CREATE TABLE IF NOT EXISTS `bank_statements` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `account_name` varchar(50) DEFAULT 'checking',
+  `amount` int(11) DEFAULT NULL,
+  `reason` varchar(50) DEFAULT NULL,
+  `statement_type` enum('deposit','withdraw') DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.bank_statements: ~0 rows (approximately)
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.bans
+CREATE TABLE IF NOT EXISTS `bans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `license` varchar(50) DEFAULT NULL,
+  `discord` varchar(50) DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `expire` int(11) DEFAULT NULL,
+  `bannedby` varchar(255) NOT NULL DEFAULT 'LeBanhammer',
+  PRIMARY KEY (`id`),
+  KEY `license` (`license`),
+  KEY `discord` (`discord`),
+  KEY `ip` (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.bans: ~0 rows (approximately)
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.crypto
+CREATE TABLE IF NOT EXISTS `crypto` (
+  `crypto` varchar(50) NOT NULL DEFAULT 'qbit',
+  `worth` int(11) NOT NULL DEFAULT 0,
+  `history` text DEFAULT NULL,
+  PRIMARY KEY (`crypto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.crypto: ~1 rows (approximately)
+INSERT INTO `crypto` (`crypto`, `worth`, `history`) VALUES
+	('qbit', 996, '[{"PreviousWorth":1000,"NewWorth":998},{"PreviousWorth":998,"NewWorth":996}]');
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.crypto_transactions
+CREATE TABLE IF NOT EXISTS `crypto_transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `message` varchar(50) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.crypto_transactions: ~0 rows (approximately)
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.dealers
+CREATE TABLE IF NOT EXISTS `dealers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  `coords` longtext DEFAULT NULL,
+  `time` longtext DEFAULT NULL,
+  `createdby` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.dealers: ~0 rows (approximately)
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.gloveboxitems
+CREATE TABLE IF NOT EXISTS `gloveboxitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(8) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.gloveboxitems: ~0 rows (approximately)
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.houselocations
+CREATE TABLE IF NOT EXISTS `houselocations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `coords` text DEFAULT NULL,
+  `owned` tinyint(1) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `tier` tinyint(4) DEFAULT NULL,
+  `garage` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.houselocations: ~0 rows (approximately)
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.house_plants
+CREATE TABLE IF NOT EXISTS `house_plants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `building` varchar(50) DEFAULT NULL,
+  `stage` int(11) DEFAULT 1,
+  `sort` varchar(50) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `food` int(11) DEFAULT 100,
+  `health` int(11) DEFAULT 100,
+  `progress` int(11) DEFAULT 0,
+  `coords` text DEFAULT NULL,
+  `plantid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `building` (`building`),
+  KEY `plantid` (`plantid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.house_plants: ~0 rows (approximately)
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.lapraces
+CREATE TABLE IF NOT EXISTS `lapraces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `checkpoints` text DEFAULT NULL,
+  `records` text DEFAULT NULL,
+  `creator` varchar(50) DEFAULT NULL,
+  `distance` int(11) DEFAULT NULL,
+  `raceid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `raceid` (`raceid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.lapraces: ~0 rows (approximately)
 
--- Data exporting was unselected.
+-- Dumping structure for table upstorydb.management_outfits
+CREATE TABLE IF NOT EXISTS `management_outfits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_name` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `minrank` int(11) NOT NULL DEFAULT 0,
+  `name` varchar(50) NOT NULL DEFAULT 'Cool Outfit',
+  `gender` varchar(50) NOT NULL DEFAULT 'male',
+  `model` varchar(50) DEFAULT NULL,
+  `props` varchar(1000) DEFAULT NULL,
+  `components` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table upstorydb.management_outfits: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.occasion_vehicles
+CREATE TABLE IF NOT EXISTS `occasion_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `seller` varchar(50) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
+  `plate` varchar(50) DEFAULT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `mods` text DEFAULT NULL,
+  `occasionid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `occasionId` (`occasionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.occasion_vehicles: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.phone_gallery
+CREATE TABLE IF NOT EXISTS `phone_gallery` (
+  `citizenid` varchar(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.phone_gallery: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.phone_invoices
+CREATE TABLE IF NOT EXISTS `phone_invoices` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `amount` int(11) NOT NULL DEFAULT 0,
+  `society` tinytext DEFAULT NULL,
+  `sender` varchar(50) DEFAULT NULL,
+  `sendercitizenid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.phone_invoices: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.phone_messages
+CREATE TABLE IF NOT EXISTS `phone_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `number` varchar(50) DEFAULT NULL,
+  `messages` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `number` (`number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.phone_messages: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.phone_tweets
+CREATE TABLE IF NOT EXISTS `phone_tweets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `firstName` varchar(25) DEFAULT NULL,
+  `lastName` varchar(25) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `date` datetime DEFAULT current_timestamp(),
+  `url` text DEFAULT NULL,
+  `picture` varchar(512) DEFAULT './img/default.png',
+  `tweetId` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.phone_tweets: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.players
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(11) NOT NULL,
+  `cid` int(11) DEFAULT NULL,
+  `license` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `money` text NOT NULL,
+  `charinfo` text DEFAULT NULL,
+  `job` text NOT NULL,
+  `gang` text DEFAULT NULL,
+  `position` text NOT NULL,
+  `metadata` text NOT NULL,
+  `inventory` longtext DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`citizenid`),
+  KEY `id` (`id`),
+  KEY `last_updated` (`last_updated`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.players: ~1 rows (approximately)
+INSERT INTO `players` (`id`, `citizenid`, `cid`, `license`, `name`, `money`, `charinfo`, `job`, `gang`, `position`, `metadata`, `inventory`, `last_updated`) VALUES
+	(1, 'BKD55994', 1, 'license:d1fabd5c6707fdb5ad9f4d6adc03d5b10d7fb139', 'sugawy', '{"bank":5020,"cash":500,"crypto":0}', '{"phone":"5289346568","gender":1,"nationality":"Portugal","lastname":"Raven","backstory":"placeholder backstory","firstname":"Tokyo","cid":1,"birthdate":"2001-11-18","account":"US02QBCore3381368599"}', '{"type":"none","isboss":false,"label":"Civilian","payment":10,"grade":{"level":0,"name":"Freelancer"},"onduty":true,"name":"unemployed"}', '{"isboss":false,"grade":{"level":0,"name":"none"},"name":"none","label":"No Gang Affiliaton"}', '{"x":-1323.9033203125,"y":-878.8087768554688,"z":13.3905029296875}', '{"thirst":84.80000000000001,"walletid":"QB-37492843","craftingrep":0,"phonedata":{"InstalledApps":[],"SerialNumber":14625933},"status":[],"jobrep":{"trucker":0,"taxi":0,"tow":0,"hotdog":0},"isdead":false,"bloodtype":"O-","hunger":83.19999999999999,"inlaststand":false,"armor":0,"attachmentcraftingrep":0,"stress":0,"jailitems":[],"ishandcuffed":false,"licences":{"business":false,"weapon":false,"driver":true},"criminalrecord":{"hasRecord":false},"fitbit":[],"fingerprint":"bq619J95lnw4784","callsign":"NO CALLSIGN","inside":{"apartment":[]},"injail":0,"tracker":false,"phone":[],"dealerrep":0}', '[{"amount":1,"name":"driver_license","type":"item","slot":1,"info":{"firstname":"Tokyo","lastname":"Raven","type":"Class C Driver License","birthdate":"2001-11-18"}},{"amount":1,"name":"phone","type":"item","slot":2,"info":[]},{"amount":1,"name":"id_card","type":"item","slot":3,"info":{"lastname":"Raven","birthdate":"2001-11-18","gender":1,"firstname":"Tokyo","nationality":"Portugal","citizenid":"BKD55994"}}]', '2024-02-18 23:16:39');
+
+-- Dumping structure for table upstorydb.playerskins
+CREATE TABLE IF NOT EXISTS `playerskins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `skin` text NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `active` (`active`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dumping data for table upstorydb.playerskins: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.player_contacts
+CREATE TABLE IF NOT EXISTS `player_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `number` varchar(50) DEFAULT NULL,
+  `iban` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.player_contacts: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.player_houses
+CREATE TABLE IF NOT EXISTS `player_houses` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `house` varchar(50) NOT NULL,
+  `identifier` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `keyholders` text DEFAULT NULL,
+  `decorations` text DEFAULT NULL,
+  `stash` text DEFAULT NULL,
+  `outfit` text DEFAULT NULL,
+  `logout` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `house` (`house`),
+  KEY `citizenid` (`citizenid`),
+  KEY `identifier` (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.player_houses: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.player_mails
+CREATE TABLE IF NOT EXISTS `player_mails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `sender` varchar(50) DEFAULT NULL,
+  `subject` varchar(50) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `read` tinyint(4) DEFAULT 0,
+  `mailid` int(11) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  `button` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.player_mails: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.player_outfits
+CREATE TABLE IF NOT EXISTS `player_outfits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `outfitname` varchar(50) NOT NULL DEFAULT '0',
+  `model` varchar(50) DEFAULT NULL,
+  `props` varchar(1000) DEFAULT NULL,
+  `components` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `citizenid_outfitname_model` (`citizenid`,`outfitname`,`model`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.player_outfits: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.player_outfit_codes
+CREATE TABLE IF NOT EXISTS `player_outfit_codes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `outfitid` int(11) NOT NULL,
+  `code` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `FK_player_outfit_codes_player_outfits` (`outfitid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.player_outfit_codes: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.player_vehicles
+CREATE TABLE IF NOT EXISTS `player_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `license` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(11) DEFAULT NULL,
+  `vehicle` varchar(50) DEFAULT NULL,
+  `hash` varchar(50) DEFAULT NULL,
+  `mods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `plate` varchar(8) NOT NULL,
+  `fakeplate` varchar(8) DEFAULT NULL,
+  `garage` varchar(50) DEFAULT NULL,
+  `fuel` int(11) DEFAULT 100,
+  `engine` float DEFAULT 1000,
+  `body` float DEFAULT 1000,
+  `state` int(11) DEFAULT 1,
+  `depotprice` int(11) NOT NULL DEFAULT 0,
+  `drivingdistance` int(50) DEFAULT NULL,
+  `status` text DEFAULT NULL,
+  `balance` int(11) NOT NULL DEFAULT 0,
+  `paymentamount` int(11) NOT NULL DEFAULT 0,
+  `paymentsleft` int(11) NOT NULL DEFAULT 0,
+  `financetime` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `plate` (`plate`),
+  KEY `citizenid` (`citizenid`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.player_vehicles: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.player_warns
+CREATE TABLE IF NOT EXISTS `player_warns` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `senderIdentifier` varchar(50) DEFAULT NULL,
+  `targetIdentifier` varchar(50) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `warnId` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.player_warns: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.stashitems
+CREATE TABLE IF NOT EXISTS `stashitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stash` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`stash`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.stashitems: ~0 rows (approximately)
+
+-- Dumping structure for table upstorydb.trunkitems
+CREATE TABLE IF NOT EXISTS `trunkitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(8) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table upstorydb.trunkitems: ~0 rows (approximately)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
